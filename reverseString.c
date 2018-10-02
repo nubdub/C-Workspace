671bc08ab5c49b00117e62b3c7f4233bc090b00f
Nabdeep Shrestha <nabdeep@vt.edu>
2018-10-02 00:24:33 -0400

    Added another implementation for reversing the string

--- reverseString.c
+++ reverseString.c
@@ -10,6 +10,7 @@ Code, Compile, Run and Debug online from anywhere in world.
 #include <string.h>
 
 void reverse(char* str, int length);
+char* reverseA(char* str, int length);
 
 int main()
 {
@@ -17,14 +18,25 @@ int main()
     int length = strlen(str);
     reverse(str, length);
     printf("%s\n", str);
+    printf("%s\n", reverseA(str, length));
 }
 
 void reverse(char* str, int length) {
-    for (int i = 0; i < (length - 1) / 2; i++) {
+    for (int i = 0; i < (length) / 2; i++) {
         char a = str[i];
         char b = str[length - 1 - i];
         str[length - 1 - i] = a;
         str[i] = b;
     }
     // return str;
+}
+
+char* reverseA(char* str, int length) {
+    char* reverse= malloc(length * sizeof(char));
+    int counter = length - 1;
+    for (int i = 0; i < length; i++) {
+        reverse[i] = str[counter];
+        counter--;
+    }
+    return reverse;
 }
